fa5769448a0bd99f0f8bc2b94024d5bc13e34c56
Piseth Ing <piseth.ing@allianz.com>
2023-07-02 21:41:18 +0700

    week 3

--- src/main/java/com/piseth/java/school/phoneshop_eng/controllers/BrandController.java
+++ src/main/java/com/piseth/java/school/phoneshop_eng/controllers/BrandController.java
@@ -1,14 +1,17 @@
 package com.piseth.java.school.phoneshop_eng.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.function.EntityResponse;
 
 import com.piseth.java.school.phoneshop_eng.dtos.BrandDTO;
 import com.piseth.java.school.phoneshop_eng.dtos.BrandResponseDTO;
@@ -16,6 +19,9 @@ import com.piseth.java.school.phoneshop_eng.mappers.BrandMapper;
 import com.piseth.java.school.phoneshop_eng.models.Brand;
 import com.piseth.java.school.phoneshop_eng.services.BrandService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/brands")
 public class BrandController {
@@ -33,5 +39,20 @@ public class BrandController {
 	public ResponseEntity<BrandResponseDTO> getOneById(@PathVariable("myId") Long id){
 		Brand brand = brandService.getById(id);
 		return ResponseEntity.ok(BrandMapper.toBrandResponseDTO(brand));
+	}
+	
+	@PutMapping("{brandId}")
+	public ResponseEntity<?> updateById(@PathVariable Long brandId, @RequestBody BrandDTO dto) {
+		log.info("Update brand with id = {}", brandId);
+		Brand brand = brandService.update(brandId, dto);
+		return ResponseEntity.ok(BrandMapper.toBrandResponseDTO(brand));
+	}
+	
+	@DeleteMapping("{brandId}")
+	public ResponseEntity<?> deleteById(@PathVariable Long brandId){
+		log.info("Delete brand with id = {}", brandId);
+		brandService.delete(brandId);
+		//return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/piseth/java/school/phoneshop_eng/services/BrandService.java
+++ src/main/java/com/piseth/java/school/phoneshop_eng/services/BrandService.java
@@ -1,9 +1,16 @@
 package com.piseth.java.school.phoneshop_eng.services;
 
+import com.piseth.java.school.phoneshop_eng.dtos.BrandDTO;
 import com.piseth.java.school.phoneshop_eng.models.Brand;
 
 public interface BrandService {
 	Brand save(Brand model);
 	
 	Brand getById(Long id);
+	// @TODO put , delete, list
+	
+	Brand update(Long brandId, BrandDTO dto);
+	
+	void delete(Long brandId);
+	
 }
